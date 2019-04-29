4926f41d0e99e59b5162cb5f3bf17e4b46d1da30
darmonlyonelab <manusporn.f@ku.th>
2019-04-29 16:03:48 +0700

    Add controller and gameplay function

--- src/GameController.java
+++ src/GameController.java
@@ -1,2 +1,365 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GameController {
+
+    @FXML
+    private Label table2_score;
+
+    @FXML
+    private Button table2_btn;
+
+    @FXML
+    private Label table3_score;
+
+    @FXML
+    private Button table3_btn;
+
+    @FXML
+    private Label table4_score;
+
+    @FXML
+    private Button table4_btn;
+
+    @FXML
+    private Label table5_score;
+
+    @FXML
+    private Button table5_btn;
+
+    @FXML
+    private Label table6_score;
+
+    @FXML
+    private Button table6_btn;
+
+    @FXML
+    private Label table7_score;
+
+    @FXML
+    private Button table7_btn;
+
+    @FXML
+    private Label table8_score;
+
+    @FXML
+    private Button table8_btn;
+
+    @FXML
+    private Label table9_score;
+
+    @FXML
+    private Button table9_btn;
+
+    @FXML
+    private Label table10_score;
+
+    @FXML
+    private Button table10_btn;
+
+    @FXML
+    private Label table11_score;
+
+    @FXML
+    private Button table11_btn;
+
+    @FXML
+    private Label table12_score;
+
+    @FXML
+    private Button table12_btn;
+
+    @FXML
+    private FlowPane welcome_pane;
+
+    @FXML
+    private FlowPane gameplay_pane;
+
+    @FXML
+    private Pane question_box;
+
+    @FXML
+    private Label question_text;
+
+    @FXML
+    private Label current_time;
+
+    @FXML
+    private Label current_score;
+
+    @FXML
+    private Button answer1;
+
+    @FXML
+    private Button answer3;
+
+    @FXML
+    private Button answer2;
+
+    @FXML
+    private Button answer4;
+
+    @FXML
+    private Button giveup;
+
+    @FXML
+    private FlowPane result_pane;
+
+    @FXML
+    private Label result_score;
+
+    @FXML
+    private Label table_play;
+
+    @FXML
+    private Button newgame_btn;
+
+    @FXML
+    private Button tryagain_btn;
+
+    private int playScore;
+    private int playNumber;
+    private Random random;
+    private int answered;
+    private ArrayList<Integer> ansList;
+    private int MAXMULTI = 12;
+
+    @FXML
+    private void initialize(){
+        changeScreen(1);
+        playNumber = 1;
+        random = new Random();
+        ansList = new ArrayList<>();
+    }
+
+    private void changeScreen(int paneNo){
+        if (paneNo == 1){
+            // 1: welcome
+            welcome_pane.setVisible(true);
+            gameplay_pane.setVisible(false);
+            result_pane.setVisible(false);
+        } else if (paneNo == 2) {
+            // 2: game play
+            welcome_pane.setVisible(false);
+            gameplay_pane.setVisible(true);
+            result_pane.setVisible(false);
+        } else if (paneNo == 3) {
+            // 3: result
+            welcome_pane.setVisible(false);
+            gameplay_pane.setVisible(false);
+            result_pane.setVisible(true);
+        }
+    }
+
+    private int makeQuestion(){
+        int multiplier = random.nextInt(MAXMULTI) + 1;
+        if (ansList.size() >= MAXMULTI)
+            multiplier = random.nextInt(MAXMULTI) + MAXMULTI + 1;
+        question_text.setText(String.format("%d x %d = ??",playNumber,multiplier));
+        return playNumber * multiplier;
+    }
+
+
+    private void startPlay(int playNumber){
+        disablePlay(true);
+        changeScreen(2);
+        this.playNumber = playNumber;
+        answered = makeQuestion();
+        while (ansList.contains(answered)){
+            if (ansList.size() >= MAXMULTI*2)
+                ansList.clear();
+            answered = makeQuestion();
+        }
+        ansList.add(answered);
+        int randAnsPoint = random.nextInt(4);
+        ArrayList<Integer> randAnsList = new ArrayList<>();
+        for (int i = 0; i < 4 ; i++) {
+            int randAns = random.nextInt(playNumber*20)+1;
+            if (playNumber % 2 == 0 && randAns % 2 != 0)
+                randAns += 1;
+            while (randAnsList.contains(randAns)){
+                randAns = random.nextInt(playNumber*20)+1;
+                if (playNumber % 2 == 0 && randAns % 2 != 0)
+                    randAns += 1;
+            }
+            randAnsList.add(randAns);
+            if (i == randAnsPoint){
+                setSelectAnswer(i,answered);
+            }else {
+                setSelectAnswer(i,randAns);
+            }
+        }
+    }
+
+    private void setSelectAnswer(int i, int answer){
+        if (i == 0)
+            answer1.setText(Integer.toString(answer));
+        if (i == 1)
+            answer2.setText(Integer.toString(answer));
+        if (i == 2)
+            answer3 .setText(Integer.toString(answer));
+        if (i == 3)
+            answer4.setText(Integer.toString(answer));
+    }
+
+    private void updateScore(int score){
+        current_score.setText("Score: " + score);
+    }
+
+    private void resetGamePlay(){
+        current_score.setText("Score: 0");
+        playScore = 0;
+        ansList.clear();
+    }
+
+    private void disablePlay(boolean bool){
+        table2_btn.setDisable(bool);
+        table3_btn.setDisable(bool);
+        table4_btn.setDisable(bool);
+        table5_btn.setDisable(bool);
+        table6_btn.setDisable(bool);
+        table7_btn.setDisable(bool);
+        table8_btn.setDisable(bool);
+        table9_btn.setDisable(bool);
+        table10_btn.setDisable(bool);
+        table11_btn.setDisable(bool);
+        table12_btn.setDisable(bool);
+    }
+
+    private void setResult(){
+        table_play.setText("Table "+ playNumber);
+        result_score.setText("Score: " + playScore);
+    }
+
+    @FXML
+    void answerFour(ActionEvent event) {
+        if (answer4.getText().equals(Integer.toString(answered))){
+            updateScore(++playScore);
+        }else {
+
+        }
+        startPlay(playNumber);
+    }
+
+    @FXML
+    void answerThree(ActionEvent event) {
+        if (answer3.getText().equals(Integer.toString(answered))){
+            updateScore(++playScore);
+        }else {
+
+        }
+        startPlay(playNumber);
+    }
+
+    @FXML
+    void answerTwo(ActionEvent event) {
+        if (answer2.getText().equals(Integer.toString(answered))){
+            updateScore(++playScore);
+        }else {
+
+        }
+        startPlay(playNumber);
+    }
+
+    @FXML
+    void asnwerOne(ActionEvent event) {
+        if (answer1.getText().equals(Integer.toString(answered))){
+            updateScore(++playScore);
+        }else {
+
+        }
+        startPlay(playNumber);
+    }
+
+    @FXML
+    void giveUp(ActionEvent event) {
+        changeScreen(3);
+        setResult();
+    }
+
+    @FXML
+    void newGame(ActionEvent event) {
+        changeScreen(1);
+        disablePlay(false);
+    }
+
+    @FXML
+    void playEigth(ActionEvent event) {
+        resetGamePlay();
+        startPlay(8);
+    }
+
+    @FXML
+    void playEleven(ActionEvent event) {
+        resetGamePlay();
+        startPlay(11);
+    }
+
+    @FXML
+    void playFive(ActionEvent event) {
+        resetGamePlay();
+        startPlay(5);
+    }
+
+    @FXML
+    void playFour(ActionEvent event) {
+        resetGamePlay();
+        startPlay(4);
+    }
+
+    @FXML
+    void playNine(ActionEvent event) {
+        resetGamePlay();
+        startPlay(9);
+    }
+
+    @FXML
+    void playSeven(ActionEvent event) {
+        resetGamePlay();
+        startPlay(7);
+    }
+
+    @FXML
+    void playSix(ActionEvent event) {
+        resetGamePlay();
+        startPlay(6);
+    }
+
+    @FXML
+    void playTen(ActionEvent event) {
+        resetGamePlay();
+        startPlay(10);
+    }
+
+    @FXML
+    void playThree(ActionEvent event) {
+        resetGamePlay();
+        startPlay(3);
+    }
+
+    @FXML
+    void playTwelve(ActionEvent event) {
+        resetGamePlay();
+        startPlay(12);
+    }
+
+    @FXML
+    void playTwo(ActionEvent event) {
+        resetGamePlay();
+        startPlay(2);
+    }
+
+    @FXML
+    void tryAgain(ActionEvent event) {
+        resetGamePlay();
+        startPlay(playNumber);
+    }
+
 }
